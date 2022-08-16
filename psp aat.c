fced232de7df2a21050797f3d406ef267d5c6544
manyuranand <111374987+manyuranand@users.noreply.github.com>
2022-08-16 21:30:59 +0530

    Add files via upload

---
+++ psp aat.c
@@ -0,0 +1,75 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+#include<math.h>
+void main() 
+{
+    int i,j,n;
+    float A[40][40],x[40],z[40],e[40],zmax,emax;
+    printf("Enter order of matrix");
+    scanf("%d",&n);
+    printf("Enter matrix elements row wise\n");
+    for(i=1;i<=n;i++);
+    {
+        for(j=1;j<=n;j++)
+        {
+            printf("A[%d][%d]=",i,j);
+            scanf("%f",&A[i][j]);
+        }
+    }
+    printf("Enter the column vector\n");
+    for(i=1;i<=n;i++)
+    {
+        printf("x[%d]=",i);
+        scanf("%f",&x[i]);
+    }
+    do
+    {
+        for(i=1;i<=n;i++)
+        {
+            z[i]=0;
+            for(j=1;j<=n;j++)
+            {
+                z[i]=z[i]+A[i][j]*x[j];
+            }
+        }
+        zmax=fabs(z[1]);
+        for(i=2;i<=n;i++)
+        {
+            if((fabs(z[i]))>zmax)
+            zmax=fabs(z[i]);
+        }
+        for(i=1;i<=n;i++)
+        {
+            z[i]=z[i]/zmax;
+        }
+        for(i=1;i<=n;i++)
+        {
+            e[i]=0;
+            e[i]=fabs((fabs(z[i]))-(fabs(x[i])));
+        }
+        emax=e[1];
+        for(i=2;i<=n;i++)
+        {
+            if(e[i]>emax)
+            emax=e[i];
+        }
+        for(i=1;i<=n;i++)
+        {
+            x[i]=z[i];
+        }
+        }
+        while(emax>0.001);
+        printf("The required eigen value is %f\n",zmax);
+        printf("The reuired eigen vector is:\n");
+        for(i=1;i<=n;i++)
+        {
+    printf("%f\t",z[i]);
+        }
+}
